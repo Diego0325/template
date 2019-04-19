ccb57df58f1c2fb5c618bbcb78b7698f94e3b456
Diego <diego_032594@outlook.es>
2019-04-18 20:19:26 -0500

    Style added

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width = device-width, initial-scale = 1.0">
 		<title>Prueba de Git</title>
+		<link rel="stylesheet" href="assets/css/main.css">
 	</head>
 
 	<body>
 		<h1>Esta es una prueba de Git</h1>
-		<h3>V1</h3>
+		<h3>V1.1</h3>
 	</body>
 </html>
