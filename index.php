d85556f06591854530a555695fe6677928ec7c85
Diego <diego_032594@outlook.es>
2019-04-18 19:50:20 -0500

    Initial template

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1.0">
+		<title>Prueba de Git</title>
+	</head>
+
+	<body>
+		<h1>Esta es una prueba de Git</h1>
+		<h3>V1</h3>
+	</body>
+</html>
