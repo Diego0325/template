f268e4e3bbbf34eee25a643ec75f134f17b3eb4e
Diego Castillo <9290747+Diego0325@users.noreply.github.com>
2019-04-26 23:26:00 -0500

    Testing branch ALIGN

--- index.php
+++ index.php
@@ -9,10 +9,14 @@
 	</head>
 
 	<body>
-		<h1>Esta es una prueba de Git</h1>
-		<h3>V1.1</h3>
-		<h4>Tengo hambre y quiero comer</h4>
-	
-		<p>Fusce gravida, dolor quis convallis mattis, enim justo porta quam, sit amet fermentum urna sapien ac diam. Suspendisse id semper mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ac condimentum mi, id auctor nibh. Curabitur fermentum sollicitudin diam in malesuada. Aliquam scelerisque sit amet eros id gravida. Praesent eget tincidunt arcu. Morbi accumsan, ex eu finibus lobortis, neque magna dictum enim, et tempus nibh lectus sit amet nibh.</p>
+
+		<div class="wrapper">
+			<h1>Esta es una prueba de Git</h1>
+			<h3>V2.1 (align)</h3>
+			<h4>We're currently at the <b>align</b> branch</h4>
+		
+			<p>Fusce gravida, dolor quis convallis mattis, enim justo porta quam, sit amet fermentum urna sapien ac diam. Suspendisse id semper mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ac condimentum mi, id auctor nibh. Curabitur fermentum sollicitudin diam in malesuada. Aliquam scelerisque sit amet eros id gravida. Praesent eget tincidunt arcu. Morbi accumsan, ex eu finibus lobortis, neque magna dictum enim, et tempus nibh lectus sit amet nibh.</p>
+		</div>
+		
 	</body>
 </html>
