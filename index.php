f810d2b5e88c0c71e507525abff0f7d75a26de5b
Diego Castillo <9290747+Diego0325@users.noreply.github.com>
2019-04-26 23:28:51 -0500

    Master branch marked

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 
 	<body>
 		<h1>Esta es una prueba de Git</h1>
-		<h3>V1.1</h3>
-		<h4>Tengo hambre y quiero comer</h4>
+		<h3>V1.1 (Master)</h3>
+		<h4>We're currently in the <b>master</b> branch</h4>
 	
 		<p>Fusce gravida, dolor quis convallis mattis, enim justo porta quam, sit amet fermentum urna sapien ac diam. Suspendisse id semper mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse ac condimentum mi, id auctor nibh. Curabitur fermentum sollicitudin diam in malesuada. Aliquam scelerisque sit amet eros id gravida. Praesent eget tincidunt arcu. Morbi accumsan, ex eu finibus lobortis, neque magna dictum enim, et tempus nibh lectus sit amet nibh.</p>
 	</body>
