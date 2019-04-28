52194660429b00ada7e5829a49ea73593579d4c5
Diego Castillo <9290747+Diego0325@users.noreply.github.com>
2019-04-27 20:35:29 -0500

    Estructura cambiada

--- aleja.php
+++ aleja.php
@@ -4,15 +4,15 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width = device-width, initial-scale = 1.0">
-		<title>Prueba de Git</title>
+		<title>Aleja</title>
 		<link rel="stylesheet" href="assets/css/main.css">
 	</head>
 
 	<body>
-		<h1>Esta es una prueba de Alejandra</h1>
+		<h1>Aleja enlazado</h1>
 		<h3>V3.1 (Alejandra)</h3>
-		<h4>We're currently in the <b>alejandra</b> branch</h4>
+		<h4>Este es un enlace</h4>
 	
-		<p>En esta rama estoy probando una funcionalidad de la que no estoy seguro. <a href="aleja.php">ALEJA</a></p>
+		<p>En esta rama estoy probando una funcionalidad de la que no estoy seguro.</p>
 	</body>
 </html>
